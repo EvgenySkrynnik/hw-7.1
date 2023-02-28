d239a4f08f6ece83dc04896e45ae1698600f6034
EvgenySkrynnik <skrynnik2011@gmail.com>
2023-02-28 23:11:35 +0100

    hw 7.1

--- src/Methods.java
+++ src/Methods.java
@@ -1,38 +1,63 @@
+import java.time.LocalDate;
+
 public class Methods {
-public static void main (String[] args) {
-    task1();
-    task2();
-    task3();
+    public static void main(String[] args) {
+    }
+
+    //Реализуйте метод, который получает в качестве параметра год,
+    // проверяет, является ли он високосным, и выводит результат в консоль.
+    //Результат программы выведите в консоль. Если год  високосный, то напечатайте сообщение:
+    // «...  год — високосный год». Если год невисокосный, то: «... год — невисокосный год».
+    public static void printLeapYear(int year) {
+        printLeapYear(year);
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год — високосный год ");
+        } else {
+            System.out.println(year + " год —не високосный год ");
+        }
+    }
+
+    //Напишите метод, куда подаются два параметра:
+    // тип операционной системы (0 — iOS, 1 — Android ) и год выпуска устройства.
+    //Если устройство старше текущего года, предложите ему установить облегченную версию.
+    //Текущий год можно получить таким способом:
+    //int currentYear = LocalDate.now().getYear();
+    //Или самим задать значение вручную — ввести в переменную числовое значение.
+    //В результате программа должна выводить в консоль сообщение,
+    // какую версию приложения (обычную или облегченную) и для какой ОС (Android или iOS)
+    // установить пользователю.
+    public static void printVersion(int os, int year) {
+        printVersion(1, 0);
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear == year && os == 1) {
+            System.out.println("установите обычную версию для Android");
+        } else if (currentYear > year && os == 1) {
+            System.out.println("установите облегченая версию для Android");
+        } else if (currentYear == year && os == 0) {
+            System.out.println("установите обычную версию для iOS");
+        } else {
+            System.out.println("установите облегченая версию для iOS");
+        }
+    }
+
+
+    //Ваша задача — доработать код, а именно написать метод,
+    // который на вход принимает дистанцию и возвращает итоговое количество дней доставки.
+
+
+    public static int howMuchDeliveryDaysNeed(int knowTheDistance ) {
+        int howMuchDeliveryDaysNeed = 0;
+        if (knowTheDistance <= 20) {
+            howMuchDeliveryDaysNeed = 1;
+        } else if (knowTheDistance > 20 && knowTheDistance <= 60) {
+            howMuchDeliveryDaysNeed++;
+        } else if (knowTheDistance > 60 && knowTheDistance <= 100) {
+            howMuchDeliveryDaysNeed++;
+        } else if (knowTheDistance > 100) {
+            return -1;
+        }
+        return howMuchDeliveryDaysNeed;
+
+    }
 }
-    private static void printLeapYear(int year) {
-}public static void task1(){
-        System.out.println( "задача 1 ");
-
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        System.out.println(year + "год является високосным");
-    } else {
-        System.out.println(year + "год является не високосным");
-
-    }}public static void task2(){
-private static void printVersion(int os,int year) {
-    int currentYear = LocalDate.now().getYear();
-    if (currentYear == year && os == 1) {
-        System.out.println("Установите обычную версию для Android");
-    } else if ( currentYear > year && os == 1) {
-        System.out.println("Установите облегченную версию для Android");
-    } else if ( currentYear == year && os == 0) {
-        System.out.println("Установите обычную версию для IOS");
-    } else {
-        System.out.println("Установите облегченную версию для IOS");
-
-    }}}}public static void task3(){
-    public static void knowTheDistance (int howMuchDeliveryDaysNeed){
-        knowTheDistance(122);
-    }var  result = howMuchDeliveryDaysNeed ;{
-            if (result == -1) {
-                System.out.println("Доставки нет");
-            }else {
-                System.out.println("Доставка займет"+result);
-            }
-        }}}
 
